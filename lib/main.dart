5006ec1f9b3f27278eb6b54719eef65de91605bb
Gibran Gamiño Espinoza <gibran_7@hotmail.es>
2023-07-29 13:13:51 -0600

    No rota al inclinar el telefono

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:dogs_app/providers/UIProvider.dart';
 import 'package:dogs_app/screens/screens.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
